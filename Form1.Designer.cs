0739eb4510d6cb22be310bf9601816e298091dcd
ITLAB1-PC31 <ITLAB1-PC31@ITLAB1-PC30>
2023-11-29 16:54:27 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace crud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.c_txtName = new System.Windows.Forms.TextBox();
+            this.c_txtAge = new System.Windows.Forms.TextBox();
+            this.c_txtGender = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.u_txtGender = new System.Windows.Forms.TextBox();
+            this.u_txtAge = new System.Windows.Forms.TextBox();
+            this.u_txtName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.c_txtGender);
+            this.groupBox1.Controls.Add(this.c_txtAge);
+            this.groupBox1.Controls.Add(this.c_txtName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(345, 186);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CREATE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FULLNAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AGE";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "GENDER";
+            // 
+            // c_txtName
+            // 
+            this.c_txtName.Location = new System.Drawing.Point(127, 37);
+            this.c_txtName.Name = "c_txtName";
+            this.c_txtName.Size = new System.Drawing.Size(203, 20);
+            this.c_txtName.TabIndex = 3;
+            this.c_txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // c_txtAge
+            // 
+            this.c_txtAge.Location = new System.Drawing.Point(127, 66);
+            this.c_txtAge.Name = "c_txtAge";
+            this.c_txtAge.Size = new System.Drawing.Size(203, 20);
+            this.c_txtAge.TabIndex = 4;
+            // 
+            // c_txtGender
+            // 
+            this.c_txtGender.Location = new System.Drawing.Point(127, 94);
+            this.c_txtGender.Name = "c_txtGender";
+            this.c_txtGender.Size = new System.Drawing.Size(203, 20);
+            this.c_txtGender.TabIndex = 5;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(255, 141);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnUpdate);
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Controls.Add(this.u_txtGender);
+            this.groupBox2.Controls.Add(this.u_txtAge);
+            this.groupBox2.Controls.Add(this.u_txtName);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(378, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(345, 186);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "UPDATE/DELETE";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(255, 141);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // u_txtGender
+            // 
+            this.u_txtGender.Location = new System.Drawing.Point(127, 94);
+            this.u_txtGender.Name = "u_txtGender";
+            this.u_txtGender.Size = new System.Drawing.Size(203, 20);
+            this.u_txtGender.TabIndex = 5;
+            // 
+            // u_txtAge
+            // 
+            this.u_txtAge.Location = new System.Drawing.Point(127, 66);
+            this.u_txtAge.Name = "u_txtAge";
+            this.u_txtAge.Size = new System.Drawing.Size(203, 20);
+            this.u_txtAge.TabIndex = 4;
+            // 
+            // u_txtName
+            // 
+            this.u_txtName.Location = new System.Drawing.Point(127, 37);
+            this.u_txtName.Name = "u_txtName";
+            this.u_txtName.Size = new System.Drawing.Size(203, 20);
+            this.u_txtName.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "GENDER";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "AGE";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "FULLNAME";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(127, 141);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 217);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(711, 286);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "READ/VIEW DATA";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(699, 261);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 515);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox c_txtGender;
+        private System.Windows.Forms.TextBox c_txtAge;
+        private System.Windows.Forms.TextBox c_txtName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox u_txtGender;
+        private System.Windows.Forms.TextBox u_txtAge;
+        private System.Windows.Forms.TextBox u_txtName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ myclass/DB.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace crud.myclass
+{
+    internal class DB
+    {
+        public MySqlConnection con;
+
+        public DB()
+        {
+            string host = "localhost";
+            string database = "studentdb";
+            string username = "root";
+            string password = "";
+            string port = "3306";
+
+            string constring = "datasource=" + host + ";database=" + database + ";port=" + port + ";username=" + username + ";" +
+                "password=" + password + ";SslMode =none;";
+
+            con = new MySqlConnection(constring);
+        }
+    }
+
+    class CRUD : DB
+    {
+        public string name { get; set; }
+
+        public string age { get; set; }
+
+        public string gender { get; set; }
+
+        public string id { get; set; }
+
+        public DataTable dt = new DataTable();
+        public DataSet ds = new DataSet();
+
+        public void Create_data()
+        {
+            con.Open();
+            using (MySqlCommand cmd = new MySqlCommand())
+            {
+                cmd.CommandText = "INSERT INTO student('name', 'age', 'gender') VALUES(name, @age, @gender) ";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = con;
+
+                cmd.Parameters.Add("name", MySqlDbType.VarChar).Value= name;
+                cmd.Parameters.Add("@age", MySqlDbType.VarString).Value = age;
+                cmd.Parameters.Add("@gender", MySqlDbType.VarChar).Value = gender;
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
+}
